e2a3046f1cb94ef35825aa86273ef42d05ea753d
Benedict JI <saxoji@gmail.com>
2024-09-19 22:59:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ USER_AGENTS = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 ]
+#'user_agent': random.choice(USER_AGENTS),
+#'source_address': '54.254.162.138',
 
 
 # 유튜브에서 영상을 다운로드하고 지정된 간격으로 오디오를 추출해 나누기
@@ -49,8 +51,6 @@ def download_video_and_split_audio(youtube_url: str, interval_minute: int) -> Li
                 'no_warnings': True,
                 'force_ipv4': True,
                 'verbose': True,
-                'user_agent': random.choice(USER_AGENTS),
-                'source_address': '54.254.162.138',
                 'http_headers': {
                     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
