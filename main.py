d7c650bd3ca08b79a5d668b4241909ac1bd43a25
Benedict JI <saxoji@gmail.com>
2024-09-19 22:40:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def download_video_and_split_audio(youtube_url: str, interval_minute: int) -> Li
                 'user_agent': random.choice(USER_AGENTS),
                 'source_address': '54.254.162.138',
                 'http_headers': {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36',
+                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                     'Accept-Language': 'en-US,en;q=0.5',
                     'Sec-Fetch-Mode': 'navigate'
