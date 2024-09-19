d61c6e54202123ea7f5dbad3992111095a14ba94
Benedict JI <saxoji@gmail.com>
2024-09-19 18:33:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,8 @@ class YouTubeAudioRequest(BaseModel):
     interval_minute: int
 
 # User-Agent 목록
-USER_AGENTS = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-]
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+
 
 # 유튜브에서 오디오를 다운로드하고 지정된 간격으로 나누기
 def download_and_split_audio(youtube_url: str, interval_minute: int) -> List[str]:
